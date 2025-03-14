41dc0da68f911b5aba6a84016934ea2e252d08ea
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-03-14 15:49:34 -0400

    A

---
+++ 00-/01-First/First_Program.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var اسم string
+	var age int
+	var gender string
+	// This is Hello World Application
+	اسم = "Sina"
+	age = 32
+	gender = "Man"
+
+	fmt.Println("Welcome", اسم)
+	fmt.Println("with Age:" , age)
+	fmt.Println("and gender:", gender)
+}
+
---
+++ 00-/01-First/go.mod
@@ -0,0 +1,3 @@
+module First_Program
+
+go 1.22.0
