288b7d9e56be7c324ea51f4b7fcdb0e9be1ad6d7
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-03-25 08:39:50 -0400

    days of week

---
+++ 00-/11-days of week/go.mod
@@ -0,0 +1,3 @@
+module days_of_week
+
+go 1.22.0
---
+++ 00-/11-days of week/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main()  {
+	const day int8 = 4
+
+	if day == 0 {
+		fmt.Println("today is Shanbe")
+	} else if day == 1 {
+		fmt.Println("today is Yekshanbe")
+	} else if day == 2 {
+		fmt.Println("today is Doshanbe")
+	} else if day == 3 {
+		fmt.Println("today is Seshanbe")
+	} else if day == 4 {
+		fmt.Println("today is Chaharshanbe")
+	} else if day == 5 {
+		fmt.Println("today is Panjshanbe")
+	} else if day == 6 {
+		fmt.Println("today is Jomee")
+	} else {
+		fmt.Println("Nadarim hamchin roozi !!!")
+
+	}
+}
