2f7b7b576958b2bb7ba824aea6503c5445aa8f29
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-03-25 09:34:42 -0400

    type of food

--- 00-/13-type of food/main.go
+++ 00-/13-type of food/main.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main()  {
-	
+	var food string = "French Fries"
+
+	switch food {
+	case "Coke", "Water", "Tea", "Coffee":
+		fmt.Println("You ordered a drink.")		
+	case "Pizza", "Burger", "Sandwich":
+		fmt.Println("You ordered a main food.")		
+	case "Salad", "French Fries", "Yogurt":
+		fmt.Println("You ordered a side dish.")		
+	default:
+		fmt.Println("Unknown food.")		
+	}
+
+
 }
