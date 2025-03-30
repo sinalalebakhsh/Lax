cc60fd1e76033f65070ff2bf8e18f95161c3afcc
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-03-30 09:14:06 -0400

    create function for sum numbers

---
+++ 00-/31-create function for sum numbers/go.mod
@@ -0,0 +1,3 @@
+module create_function_for_sumnumbers
+
+go 1.22.0
---
+++ 00-/31-create function for sum numbers/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func sum_numbers(number_one, number_two int)  {
+	fmt.Println(number_one + number_two)
+}
+
+func main()  {
+	sum_numbers(2 , 4)
+}
