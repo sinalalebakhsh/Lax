b079344c0008d4c104d349421daf2aef6501453e
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-04-01 23:17:07 +0330

    returning multiple parameters as output

--- 00-/33-Returning multiple parameters as output/main.go
+++ 00-/33-Returning multiple parameters as output/main.go
@@ -1,14 +1,17 @@
 package main
 
+import "fmt"
+
 const pi = 3.14
 
-func AreaAndPerimeter(r float64) (float64, float64)  {
+func AreaAndPerimeter(r float64) (float64, float64) {
 	perimeter := pi * (r * 2)
 	area := pi * (r * r)
-	return perimeter
+	return perimeter, area
 }
 
-func main()  {
+func main() {
 
-	
+	fmt.Println(AreaAndPerimeter(1))
+
 }
