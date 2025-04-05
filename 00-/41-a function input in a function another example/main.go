48aa1028ba6dbafa4386f4d1362df3be3c5dfc49
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-04-05 17:28:28 +0330

    a function input in a function another example

--- 00-/41-a function input in a function another example/main.go
+++ 00-/41-a function input in a function another example/main.go
@@ -10,6 +10,11 @@ func square(number_ int) int {
 	return number_ * number_
 }
 
+func mohite_moraba(zele_moraba int) int {
+	return zele_moraba * 4
+}
+
+
 func apply_function(numbers int, functions func(int) int) {
 	result := functions(numbers)
 	fmt.Println(result)
@@ -19,4 +24,8 @@ func main() {
 	apply_function(2 , double)   
 
 	apply_function(3, square)
+
+	apply_function(5, mohite_moraba)
+
+
 }
