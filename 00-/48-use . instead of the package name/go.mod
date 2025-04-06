6783fcaf29ce0f5d632a6b2526c7e106dfaa766b
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-04-06 05:33:25 -0400

    use . instead of the package name

---
+++ 00-/48-use . instead of the package name/go.mod
@@ -0,0 +1,3 @@
+module use_._instead_of_the_package_name
+
+go 1.24.2
---
+++ 00-/48-use . instead of the package name/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	. "fmt"
+)
+
+func main()  {
+	Println("use . instead of the package name = fmt")
+}
