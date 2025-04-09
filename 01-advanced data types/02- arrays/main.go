fe8d6079f45db5ed9e9b64eb55240c0d68a669dd
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-04-09 07:50:36 -0400

    arrays

---
+++ 01-advanced data types/02- arrays/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	student_names := [20]string{}
+
+	var student_scores [20]int
+
+	// default values is " " a empty string
+	fmt.Println(student_names)
+
+	// default value is 0
+	fmt.Println(student_scores)
+
+}
