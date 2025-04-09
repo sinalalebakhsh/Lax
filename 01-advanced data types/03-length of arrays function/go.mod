4ccbfff34895da8d5cb625fb0dbc277a97b11a67
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-04-09 07:58:54 -0400

    length of arrays function

---
+++ 01-advanced data types/03-length of arrays function/go.mod
@@ -0,0 +1,3 @@
+module length_of_arrays_function
+
+go 1.24.2
---
+++ 01-advanced data types/03-length of arrays function/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	var student_scores [20]int
+
+	fmt.Println(len(student_scores))
+	
+}
