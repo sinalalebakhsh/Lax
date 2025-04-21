3487d47f133f6a190becebe832752fa5c917fe1b
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-04-21 12:18:53 -0400

    clear a slice values

---
+++ 01-advanced data types/18-clear a slice values/go.mod
@@ -0,0 +1,3 @@
+module clear_a_slice_values
+
+go 1.24.2
---
+++ 01-advanced data types/18-clear a slice values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	numbers := []int{1,2,1233,4,5,612}
+	fmt.Println(numbers)
+
+
+	clear(numbers)
+	fmt.Println(numbers)
+
+	
+}
