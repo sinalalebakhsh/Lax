fffd9357f332a498ee8a33b24caef39b18324cee
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-04-28 11:54:16 -0400

    reflect - TypeOf

--- 02-Subspecialty/00- reflect - TypeOf/main.go
+++ 02-Subspecialty/00- reflect - TypeOf/main.go
@@ -5,10 +5,26 @@ import (
 	"reflect"
 )
 
-func main()  {
-	numbers := []int{1,2,3,4}
-	copy_to_numbers := copy(numbers, []int{77, 99})
+func main() {
+	src := []int{10, 11, 12, 13, 14}
+	dst := []int{0, 1, 2, 3, 4}
+	n := copy(dst, src)
+	fmt.Println("n =", n, "src =", src, "dst =", dst)
+	
+	
+	
+	
 
-	fmt.Println(reflect.TypeOf(copy_to_numbers))	
+	numbers := []int{666,888, 555, 222, 12, 13, 13 ,15}
+	destination_numbers := []int{1, 0, 1, 0}
+	number_of_destination_numbers := copy(destination_numbers, numbers)
+	fmt.Println("numbers:",numbers)
+	fmt.Println("number_of_destination_numbers:", number_of_destination_numbers)
+	fmt.Println("destination_numbers:",destination_numbers)
+	
+	
 
+	fmt.Println(reflect.TypeOf(numbers))
+	fmt.Println(reflect.TypeOf(destination_numbers))
+	fmt.Println(reflect.TypeOf(number_of_destination_numbers))
 }
