249a89bf3cfbc8ebf2cd966dafeeda1984552e68
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-05-07 14:10:34 -0400

    struct and method togather

--- 01-advanced data types/31- struct and method togather/main.go
+++ 01-advanced data types/31- struct and method togather/main.go
@@ -1,11 +1,29 @@
 package main
 
+import "fmt"
+
 type user struct {
 	first_name string
 	last_name  string
 	age        int
 }
 
-func main() {
+func (u user) full_name() string  {
+	return u.first_name + " " + u.last_name
+}
 
+func (u user) print()  {
+	fmt.Println("Name:", u.full_name())
+	fmt.Println("Age:", u.age)
+}
+
+func main() {
+	sina := user{
+		first_name: "sina",
+		last_name: "lalehbakhsh",
+		age: 33,
+	}
+
+	sina.print()
+	
 }
