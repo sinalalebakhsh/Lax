521879f23abbd1211eae64f93f82d5e1ce331c0f
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-05-12 01:14:13 -0400

    d_a_v_w_t_o_pointer

--- 01-advanced data types/34- define a variable with type of pointer/main.go
+++ 01-advanced data types/34- define a variable with type of pointer/main.go
@@ -7,5 +7,10 @@ func main()  {
 	var address_of_number_in_memory *int = &number
 	fmt.Println("number:", number)
 	fmt.Println("address of number in memory:", address_of_number_in_memory)
+
+	var a_number *int 
+	var b_number *int = a_number
+	fmt.Println("number:", a_number)
+	fmt.Println("number:", b_number)
 	
 }
