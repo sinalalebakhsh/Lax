bef9df9a7b57970a624f4f85afd69467ced54ce6
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-05-14 09:53:09 -0400

    pointer with method

--- 01-advanced data types/38- pointer with method/main.go
+++ 01-advanced data types/38- pointer with method/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main()  {
-	
+import "fmt"
+
+type player struct {
+	name string
+	hp   int
+}
+
+func (p *player) hit() {
+	p.hp--
+}
+
+func main() {
+	player1 := player{
+		name: "sina",
+		hp: 100,
+	}
+	fmt.Println("before hit:", player1.hp)
+	player1.hit()
+	fmt.Println("after hit:", player1.hp)
+
 }
