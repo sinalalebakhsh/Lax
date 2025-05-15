95bd6fc62fe3335828f961d6ed5a12dc1ceca9fb
sina lalehbakhsh <lalebakhshsina@gmail.com>
2025-05-15 08:47:51 -0400

    concurrency

--- 03-concurrency/01-first/main.go
+++ 03-concurrency/01-first/main.go
@@ -1,8 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 
 
 func main()  {
-	
+	print_letters()
+	fmt.Println()
+	print_numbers()
+	fmt.Println()
 }
 
+func print_letters()  {
+	for i := 'A'; i <= 'Z'; i++ {
+		fmt.Print(string(i), " ")
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+func print_numbers()  {
+	for i := 1; i <= 50; i++ {
+		fmt.Print(i, " ")
+		time.Sleep(time.Millisecond * 50)
+	}
+}
