1e6b4f1631de7cbd0bacea9ff45ef0fa84383a81
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-05-18 12:52:01 +0330

    Waitgroup add plus

---
+++ 00-Base/03-concurrency/05-Waitgroup add plus/go.mod
@@ -0,0 +1,3 @@
+module __A_Waitgroup_
+
+go 1.24.1
---
+++ 00-Base/03-concurrency/05-Waitgroup add plus/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func print_letters()  {
+	for i := 'A'; i <= 'Z'; i++ {
+		fmt.Print(string(i), " ")
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+func print_numbers()  {
+	for i := 1; i <= 50; i++ {
+		fmt.Print(i, " ")
+		time.Sleep(time.Millisecond * 50)
+	}
+}
+
+
+func main(){
+	var wait_group sync.WaitGroup
+	wait_group.Add(3)
+
+	go func()  {
+		print_letters()
+		wait_group.Done()
+	}()
+
+	go func()  {
+		print_numbers()	
+		wait_group.Done()
+	}()
+
+	go func()  {
+		fmt.Println("finished")
+		wait_group.Done()
+	}()
+	
+
+	wait_group.Wait()
+	
+		
+}
