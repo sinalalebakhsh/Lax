2d0bb831106c8b4479953701ee16d1e77cce4935
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-05-18 15:16:26 +0330

    without delay Goroutine with Waitgroup

--- 00-Base/03-concurrency/06-without delay Goroutine with Waitgroup/main.go
+++ 00-Base/03-concurrency/06-without delay Goroutine with Waitgroup/main.go
@@ -1,21 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	var a, b, c int
 
+	var wait_group sync.WaitGroup
+
+	wait_group.Add(3)
+
+
+
 	go func() {
 		a = 1
+		wait_group.Done()
 	}()
 
 	go func() {
 		b = 2
+		wait_group.Done()
 	}()
 
 	go func() {
 		c = 3
+		wait_group.Done()
 	}()
+	
+	wait_group.Wait()
 
 	fmt.Println(a, b, c)
 }
