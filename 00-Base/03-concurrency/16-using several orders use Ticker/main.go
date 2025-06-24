85850109fbf5ea25d61d9a33f0b11e4ddfec9331
Sina Lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2025-06-24 20:02:21 +0330

    using several orders use Ticker

---
+++ 00-Base/03-concurrency/16-using several orders use Ticker/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	coffee_chanel := make(chan string)
+	tea_chanel := make(chan string)
+	food_chanel := make(chan string)
+	time_out_channel := time.After(3 * time.Second)
+	ticker_channel := time.Tick(time.Millisecond * 500)
+	
+	go func()  {
+		time.Sleep( time.Second)
+		tea_chanel <- "Black Tea"
+	}()
+	
+
+	go func()  {
+		time.Sleep(2 *time.Second)
+		coffee_chanel <- "Esspresso coffee"
+	}()
+
+
+
+	defer fmt.Println("Berid khoonatoon Shop is closed!")
+
+	for {
+		select {
+		case coffee := <-coffee_chanel:
+			fmt.Printf("Received a coffee order: %s\n", coffee)
+		case tea := <-tea_chanel:
+			fmt.Printf("Received a tea order: %s\n", tea)
+		case food := <-food_chanel:
+			fmt.Printf("Received a food order: %s\n", food)
+		case <-time_out_channel:
+			return
+		case <-ticker_channel:
+			fmt.Println("Tick!")
+		default:
+			fmt.Print()
+		}
+	}
+
+
+}
